b07c92cd523ac309826d7902b807c2e06bab00e0
lwwatts <lwwatts@smith.edu>
2025-02-18 13:58:24 -0500

    Added passenger name to the passenger print outs

--- Passenger.java
+++ Passenger.java
@@ -9,14 +9,20 @@ public class Passenger implements PassengerRequirements{
     public void boardCar(Car c){
         Boolean boarded = c.addPassenger(this);
         if(!boarded){
-            System.out.println("Error: Passenger cannot board this car because this car is full.");
+            System.out.println("Error: Passenger " + this.name + " cannot board this car because this car is full.");
+        }
+        else{
+            System.out.println("Passenger " + this.name + " has boarded the car.");
         }
     }
 
     public void getOffCar(Car c){
         Boolean gotOff = c.removePassenger(this);
         if(!gotOff){
-            System.out.println("Error: Passenger cannot get off this car because the passenger is not in this car.");
+            System.out.println("Error: Passenger " + this.name + " cannot get off this car because they are not in this car.");
+        }
+        else{
+            System.out.println("Passenger " + this.name + " has gotten off the car.");
         }
     }
 
