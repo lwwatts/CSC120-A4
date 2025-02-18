03fa2f024e54c0af63ba6081a074dd75466ba177
lwwatts <lwwatts@smith.edu>
2025-02-18 13:29:31 -0500

    Passenger class written but not tested

--- Passenger.java
+++ Passenger.java
@@ -1,8 +1,26 @@
-public class Passenger {
+public class Passenger implements PassengerRequirements{
     
     private String name;
 
     public Passenger(String name) {
         this.name = name;
+    }
+
+    public void boardCar(Car c){
+        Boolean boarded = c.addPassenger(this);
+        if(!boarded){
+            System.out.println("Error: Passenger cannot board this car because this car is full.");
+        }
+    }
+
+    public void getOffCar(Car c){
+        Boolean gotOff = c.removePassenger(this);
+        if(!gotOff){
+            System.out.println("Error: Passenger cannot get off this car because the passenger is not in this car.");
+        }
+    }
+
+    public String getName(){
+        return this.name;
     }
 }
