4e534dadf9a00cb16bbbd3caf9463be0e952d8c3
lwwatts <lwwatts@smith.edu>
2025-02-20 11:19:20 -0500

    Added javadocs to Train class

--- Train.java
+++ Train.java
@@ -6,6 +6,13 @@ public class Train implements TrainRequirements{
     Engine engine;
     ArrayList<Car> cars;
 
+    /**
+     * Constructor for a new Train object
+     * @param fuelType the type of fuel the train's engine uses
+     * @param fuelCapacity  the maximum amount of fuel the train's engine can hold
+     * @param nCars the number of cars that the train has (not including the engine)
+     * @param passengerCapacity the number of passengers that each car on the train can hold
+     */
     public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity){
         this.engine = new Engine(fuelType, fuelCapacity, fuelCapacity);
         this.cars = new ArrayList<>();
@@ -15,14 +22,27 @@ public class Train implements TrainRequirements{
         }
     }
     
+    /**
+     * An accessor method for the train's engine
+     * @return Engine: the engine associated with this train
+     */
     public Engine getEngine(){
         return this.engine;
     }
 
+    /**
+     * An accessor method for the train's cars
+     * @param i the index of the car on the train (where the first car is index 0)
+     * @return Car: the car at the given index in this train's list of cars
+     */
     public Car getCar(int i){
         return this.cars.get(i);
     }
 
+    /**
+     * Determines how many total passengers can be seated on this train
+     * @return int: the max number of passengers this train can seat
+     */
     public int getMaxCapacity(){
         int capacity = 0;
         Iterator<Car> iter = this.cars.iterator();
@@ -33,6 +53,10 @@ public class Train implements TrainRequirements{
         return capacity;
     }
     
+    /**
+     * Determines the number of empty seats left on this train
+     * @return int: the sum of all the empty seats in each of this train's cars
+     */
     public int seatsRemaining(){
         int seatsLeft = 0;
         Iterator<Car> iter = this.cars.iterator();
@@ -43,6 +67,9 @@ public class Train implements TrainRequirements{
         return seatsLeft;
     }
 
+    /**
+     * Prints out the list of cars and their passengers
+     */
     public void printManifest(){
         Iterator<Car> iter = this.cars.iterator();
         int count = 0;
@@ -55,6 +82,6 @@ public class Train implements TrainRequirements{
     }
 
     public static void main(String[] args){
-        
+
     }
 }
