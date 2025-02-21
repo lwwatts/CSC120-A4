60559a27f57082e2f073f1bbce1991ed7303b3be
lwwatts <lwwatts@smith.edu>
2025-02-21 11:17:23 -0500

    Added javadocs to Passenger class

--- Passenger.java
+++ Passenger.java
@@ -2,10 +2,18 @@ public class Passenger implements PassengerRequirements{
     
     private String name;
 
+    /**
+     * Constructor
+     * @param name the name of the passenger
+     */
     public Passenger(String name) {
         this.name = name;
     }
 
+    /**
+     * Adds the passenger to the manifest of a given car and prints out a message about boarding status
+     * @param c the car which the passenger is to be added to
+     */
     public void boardCar(Car c){
         Boolean boarded = c.addPassenger(this);
         if(!boarded){
@@ -16,6 +24,10 @@ public class Passenger implements PassengerRequirements{
         }
     }
 
+    /**
+     * Removes the passenger from the manifest of a given car and prints out a message about getting off status
+     * @param c the car which the passenger is to be removed from
+     */
     public void getOffCar(Car c){
         Boolean gotOff = c.removePassenger(this);
         if(!gotOff){
@@ -26,6 +38,10 @@ public class Passenger implements PassengerRequirements{
         }
     }
 
+    /**
+     * Alters the String form of Passenger to be the passenger's name
+     * @return String: the passenger's name
+     */
     public String toString(){
         return this.name;
     }
